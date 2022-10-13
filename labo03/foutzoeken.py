5e99008c67c0a951cea9de60820d5c186db236ef
gino telingen <ginoterlingen@student.ap.be>
2022-10-13 14:23:52 +0200

    met fouten

---
+++ labo03/foutzoeken.py
@@ -0,0 +1,10 @@
+#Labo 3: oefening fouten maken
+In deze opdracht gaan we een paar fouten maken die jullie moeten corrigeren
+
+print("Misschien vind je hier al de eerste fout?"
+a="10"
+b="20"
+print(((a)*5)
+print((a+b)/3)
+hex("25")
+Print('Als je dit ziet in de output ben je op het einde van de opdracht geraakt. Proficiat!")
