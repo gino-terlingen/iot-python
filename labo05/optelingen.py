02dd300865339f39c51ac8d20c759e571069562f
gino telingen <ginoterlingen@student.ap.be>
2022-10-27 21:48:15 +0200

    2

--- labo05/optelingen.py
+++ labo05/optelingen.py
@@ -1,6 +1,9 @@
 
 from random import randint
+score = 0
+attempts = 0
 while True:
+    attempts += 1
     n1 = randint(2, 10)
     n2 = randint(2, 10)
     print('Wat is het product van volgende getallen?\n', n1, n2)
@@ -12,8 +15,13 @@ while True:
     product = n1 * n2
     if answer == product:
         print('joempie het is juist!')
+        score += 1
     else:
         print('fout')
+
+    if score == 2:
+        print(f"jouw score is {score}")
+        break
 
 
     
