e1846894eddefd9abb02fa75fb58d42fdc5688ca
gino <s139860@ap.be>
2023-01-04 22:32:09 +0100

    maakt een standaard grafiek

--- meteo/meteochart.py
+++ meteo/meteochart.py
@@ -1,38 +1,68 @@
 #gemaakt door gino terlingen
+import turtle as t
+import http.client as web
+import json
 
 print("hallo welkom bij deze weer grafiek")
-print ("kies een stad")
+print ("kies een land")
+print ("optie")
+print ("belgië")
+print ("duitsland")
+print ("spanje")
+print ("engeland")
+print ("schotland")
+
 stad = input("we zullen eens kijken voor: ")
 
 
-if stad == ("antwerpen"):
-    latitude = 51.22
-    longitude = 4.40
-elif  stad == ("brugge"):
-    latitude = 51.21
-    longitude = 3.22
-elif stad == ("oostende"):
-    latitude = 51.22
-    longitude = 2.93
-elif stad == ("brussel"):
+if stad == ("belgië"):
     latitude = 50.85
     longitude = 4.35
-elif stad == ("luik"):
-    latitude = 51.00
-    longitude = 3.02         
+elif  stad == ("duitsland"):
+    latitude = 52.52
+    longitude = 13.41
+elif stad == ("spanje"):
+    latitude = 40.42
+    longitude = -3.70
+elif stad == ("engeland"):
+    latitude = 51.51
+    longitude = -0.13
+elif stad == ("schotland"):
+    latitude = 55.87
+    longitude = -4.26
+else:
+    print("dit is geen optie")
+    exit()
 
 
 
-print  (latitude)
+#print  (latitude)
 
-print (longitude)          
+#print (longitude)          
 
-import http.client
 
-conn = http.client.HTTPSConnection("api.open-meteo.com")
+landUrl = f'/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=temperature_2m'
+
+
+conn = web.HTTPSConnection("api.open-meteo.com")
 payload = ''
 headers = {}
-conn.request("GET", f"/v1/forecast?hourly=temperature_2m&latitude={latitude}&longitude={longitude}", payload, headers)
+conn.request("GET", landUrl, payload, headers)
 res = conn.getresponse()
-data = res.read()
-print(data.decode("utf-8"))
+
+apiJson = json.loads(res.read())
+
+
+X = 0
+XScale = 5
+YScale = 14
+t.pencolor('#0000ff')
+t.pensize(10)
+
+t.up()
+xoffset = 500
+for Y in apiJson['hourly']['temperature_2m']:
+   t.goto((X * XScale)-xoffset, Y * YScale)
+   X += 1
+   t.down()
+t.done()
