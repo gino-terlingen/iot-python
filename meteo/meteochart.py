84f0eb626af861b77a3ed67535bb407f9a89590b
gino <s139860@ap.be>
2023-08-15 16:26:30 +0200

    code kleiner gemaakt en comentaar toe gevoegd

--- meteo/meteochart.py
+++ meteo/meteochart.py
@@ -1,17 +1,40 @@
 #gemaakt door gino terlingen
+
+#importeren van de nodige elemente
+
 import turtle as t
 import http.client as web
 import json
 
-print("hallo welkom bij deze weer grafiek")
-print ("kies een land")
-print ("optie")
-print ("belgië")
-print ("duitsland")
-print ("spanje")
-print ("engeland")
-print ("schotland")
-print ("usa")
+#tekenen van y-ass
+
+def draw_yaxis():
+
+    t.pendown()
+    t.forward(750)
+    t.setheading(270)
+    t.forward(750)
+    t.setheading(0)
+    t.forward(50)
+    t.setheading(90)
+    t.pendown()
+
+#tekenen van x-ass
+
+def draw_xaxis():
+    
+    t.pendown()
+    t.forward(950)
+    t.setheading(180)
+    t.forward(950)
+    t.setheading(90)
+    t.forward(50)
+    t.setheading(0)
+    t.pendown() 
+
+#keuze menu voor de gebruiker
+
+print("hallo welkom bij deze weer grafiek\nkies een land\noptie\nbelgië\nduitsland\nspanje\nengeland\nschotland\nusa")
 
 stad = input("we zullen eens kijken voor: ")
 
@@ -38,7 +61,7 @@ else:
     print("dit is geen optie")
     exit()
           
-
+#initialize api informatie 
 
 landUrl = f'/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=temperature_2m'
 
@@ -53,132 +76,76 @@ api = json.loads(res.read())
 
 
 X = 0
-XScale = 5
-YScale = 14
-t.pencolor('#ff0000')
-t.pensize(2)
-t.tracer(0, 0)
-t.ht()
+XScale = 6
+YScale = 9
+
+#beging turtle klaar zetten
+ 
+t.speed(100)
+t.left(90) 
+t.color('red')
+t.penup()
+t.setpos(-450,-350)
+
+#loops voor de assen tekenen
+
+for i in range(20):
+    draw_yaxis()
 
 t.penup()
-t.goto(-450.00,400.00)
-t.pendown()
-t.goto(-450.00,-300.00)
-t.goto(500.00,-300.00)
-t.penup()
-t.goto(-450.00,380.00)
-t.pendown()
-t.goto(500.00,380.00)
-t.penup()
-t.goto(-450.00,340.00)
-t.pendown()
-t.goto(500.00,340.00)
-t.penup()
-t.goto(-450.00,300.00)
-t.pendown()
-t.goto(500.00,300.00)
-t.penup()
-t.goto(-450.00,260.00)
-t.pendown()
-t.goto(500.00,260.00)
-t.penup()
-t.goto(-450.00,220.00)
-t.pendown()
-t.goto(500.00,220.00)
-t.penup()
-t.goto(-450.00,180.00)
-t.pendown()
-t.goto(500.00,180.00)
-t.penup()
-t.goto(-450.00,140.00)
-t.pendown()
-t.goto(500.00,140.00)
-t.penup()
-t.goto(-450.00,100.00)
-t.pendown()
-t.goto(500.00,100.00)
-t.penup()
-t.goto(-450.00,60.00)
-t.pendown()
-t.goto(500.00,60.00)
-t.penup()
-t.goto(-450.00,20.00)
-t.pendown()
-t.goto(500.00,20.00) 
-t.penup()
-t.goto(-450.00,-20.00)
-t.pendown()
-t.goto(500.00,-20.00)
-t.penup()
-t.goto(-450.00,-60.00)
-t.pendown()
-t.goto(500.00,-60.00)
-t.penup()
-t.goto(-450.00,-100.00)
-t.pendown()
-t.goto(500.00,-100.00)
-t.penup()
-t.goto(-450.00,-140.00)
-t.pendown()
-t.goto(500.00,-140.00)
-t.penup()
-t.goto(-450.00,-180.00)
-t.pendown()
-t.goto(500.00,-180.00)
-t.penup()
-t.goto(-450.00,-220.00)
-t.pendown()
-t.goto(500.00,-220.00)
-t.penup()
-t.goto(-450.00,-260.00)
-t.pendown()
-t.goto(500.00,-260.00)
-t.penup()
+t.setpos(-450,-350)
+t.setheading(0)
 
+for i in range(15):
+    draw_xaxis()
+
+#temperatuur tekenen
+
+H=("verdana",15,"normal")
+t.penup()
 t.pencolor('#000000')
-t.goto(-510.00,380.00)
-t.write("45°c", font=("verdana",15,"normal"))
-t.goto(-510.00,340.00)
-t.write("40°c", font=("verdana",15,"normal"))
-t.goto(-510.00,300.00)
-t.write("35°c", font=("verdana",15,"normal"))
-t.goto(-510.00,260.00)
-t.write("30°c", font=("verdana",15,"normal"))
-t.goto(-510.00,220.00)
-t.write("25°c", font=("verdana",15,"normal"))
-t.goto(-510.00,180.00)
-t.write("20°c", font=("verdana",15,"normal"))
-t.goto(-510.00,140.00)
-t.write("15°c", font=("verdana",15,"normal"))
-t.goto(-510.00,100.00)
-t.write("10°c", font=("verdana",15,"normal"))
-t.goto(-510.00,60.00)
-t.write("5°c", font=("verdana",15,"normal"))
-t.goto(-510.00,20.00)
-t.write("0°c", font=("verdana",15,"normal"))
-t.goto(-510.00,-20.00)
-t.write("-5°c", font=("verdana",15,"normal"))
-t.goto(-510.00,-60.00)
-t.write("-10°c", font=("verdana",15,"normal"))
-t.goto(-510.00,-100.00)
-t.write("-15°c", font=("verdana",15,"normal"))
-t.goto(-510.00,-140.00)
-t.write("-20°c", font=("verdana",15,"normal"))
-t.goto(-510.00,-180.00)
-t.write("-25°c", font=("verdana",15,"normal"))
-t.goto(-510.00,-220.00)
-t.write("-30°c", font=("verdana",15,"normal"))
-t.goto(-510.00,-260.00)
-t.write("-35°c", font=("verdana",15,"normal"))
-t.goto(-510.00,-300.00)
-t.write("-40°c", font=("verdana",15,"normal"))
+t.goto(-505.00,380.00)
+t.write("45°c", font=(H))
+t.setheading(270)
+t.forward(43)
+t.write("40°c", font=(H))
+t.forward(43)
+t.write("35°c", font=(H))
+t.forward(43)
+t.write("30°c", font=(H))
+t.forward(43)
+t.write("25°c", font=(H))
+t.forward(43)
+t.write("20°c", font=(H))
+t.forward(43)
+t.write("15°c", font=(H))
+t.forward(43)
+t.write("10°c", font=(H))
+t.forward(43)
+t.write("5°c", font=(H))
+t.forward(43)
+t.write("0°c", font=(H))
+t.forward(43)
+t.write("-5°c", font=(H))
+t.forward(43)
+t.write("-10°c", font=(H))
+t.forward(43)
+t.write("-15°c", font=(H))
+t.forward(43)
+t.write("-20°c", font=(H))
+t.forward(43)
+t.write("-25°c", font=(H))
+t.forward(43)
+t.write("-30°c", font=(H))
+t.forward(43)
+t.write("-35°c", font=(H))
+t.forward(43)
+t.write("-40°c", font=(H))
 
 t.goto(0.00,400.00)
-t.write(stad.upper(), font=("verdana",15,"normal"))
+t.write(stad.upper(), font=(H))
 
-t.tracer(1, 8)
-t.st()
-
+#lijn van de demperatuur tekenen
 
 t.speed(2)
 t.up()
